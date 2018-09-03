63ad3ee8b31e4e3b400e8022b4e95d24851317b5
Benjamin Huang <bhuang@rms.com>
2018-09-02 17:25:48 -0700

    login UI

--- mobile/src/commons/LoginButton.js
+++ mobile/src/commons/LoginButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Text } from 'react-native-design-utility';
 import { TouchableOpacity, Image } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from 'react-native-vector-icons';
 
 import { images } from '../constants/images';
 import { theme } from '../constants/theme';
@@ -19,26 +19,10 @@ const bgColor = type => {
 
 const LoginButton = ({ children, type, onPress }) => (
   <TouchableOpacity onPress={onPress}>
-    <Box
-      dir="row"
-      align="center"
-      shadow={1}
-      bg={bgColor(type)}
-      w="80%"
-      self="center"
-      p="2xs"
-      radius="xs"
-      mb="sm"
-    >
+    <Box dir="row" h={40} align="center" shadow={1} bg={bgColor(type)}
+      w="80%" self="center" p="2xs" radius="xs" mb="sm">
       <Box mr="sm">
-        <Box
-          bg="white"
-          h={32}
-          w={32}
-          radius="xs"
-          center
-          style={{ position: 'relative' }}
-        >
+        <Box bg="white" h={32} w={32} radius="xs" center style={{ position: 'relative', marginLeft:5 }}>
           {type === 'google' && <Image source={images.googleColorIcon} />}
           {type === 'facebook' && (
             <FontAwesome
