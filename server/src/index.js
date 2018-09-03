754bba0c40fd8f3b2e7f57dc9cbac794caf8e409
Benjamin Huang <bhuang@rms.com>
2018-09-03 09:30:15 -0700

    add auth

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 8964;
 const app = express();
 middlewaresConfig(app);
 
-app.get('/', (req, res)=>{
-  res.send("Welcome");
+app.get('/', (req, res) => {
+  res.send('Welcome');
 });
 
 app.listen(PORT, err => {
