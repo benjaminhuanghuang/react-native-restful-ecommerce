7e68d6799ddf322543863d98b56624d406519976
Benjamin Huang <bhuang@rms.com>
2018-09-03 09:02:42 -0700

    connenct to mongodb

---
+++ server/src/constants/index.js
@@ -0,0 +1,3 @@
+export const isDev = process.env.NODE_ENV === 'development';
+
+export const DB_URL = 'mongodb://admin:admin1234@ds243212.mlab.com:43212/instore-dev';
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import middlewaresConfig from './config/middlewares';
+import './config/db';
+
 const PORT = process.env.PORT || 8964;
 
 const app = express();
+middlewaresConfig(app);
 
 app.get('/', (req, res)=>{
   res.send("Welcome");
